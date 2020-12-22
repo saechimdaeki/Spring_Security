ec88f71f34b1511dc0a8dbff42b8193269a12495
김준성 <anima94@naver.com>
2020-12-22 22:52:09 +0900

    사용자 정의 보안 기능 구현

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Spring_Security
  Repository for studying Spring_Security
+
+# 1. 인증 api - 사용자 정의 보안 기능 구현
+
+![image](https://user-images.githubusercontent.com/40031858/102893725-6afa1900-44a5-11eb-8af2-f552d32a1c96.png)
+
+```
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+            .authorizeRequests()
+            .anyRequest().authenticated()
+        .and()
+            .formLogin();
+    }
+
+}
+```
---
+++ src/main/java/security/demo/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package security.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated() //요청에 대한 보안검사 시작 (인가정책)
+        .and()
+            .formLogin(); //form login 방식의 인증
+    }
+}
