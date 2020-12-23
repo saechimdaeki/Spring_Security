e6871453bc93924315cd6e85de88e51a35c8b2af
김준성 <anima94@naver.com>
2020-12-23 16:43:42 +0900

    Form Login 인증

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Spring_Security
  Repository for studying Spring_Security
 
-# 1. 인증 api - 사용자 정의 보안 기능 구현
+## 1-1). 인증 api - 사용자 정의 보안 기능 구현
 
 ![image](https://user-images.githubusercontent.com/40031858/102893725-6afa1900-44a5-11eb-8af2-f552d32a1c96.png)
 
@@ -19,5 +19,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
             .formLogin();
     }
 
+}
+```
+
+## 1-2). 인증 API - HTTP Basic인증, BasicAuthenticationFilter
+
+![image](https://user-images.githubusercontent.com/40031858/102970155-59af1c00-453a-11eb-9921-d33e526e84f8.png)
+
+
+```
+protected void configure(HttpSecurity http) throws Exception{
+    http.httpBasic();
+}
+```
+
+## 1-3) 인증 API - Form 인증
+#### http.formLogin() : Form 로그인 인증 기능이 작동함
+```
+protected void configure(HttpSecurity http) throws Exception{
+    http.formLogin()
+    .loginPage("/login.html") //사용자 정의 로그인 페이지
+    .defaultSuccessUrl("/home") // 로그인 성공 후 이동 페이지
+    .failureUrl("/login.html?error=true") // 로그인 실패 후 이동 페이지
+    .usernameParameter("username") // 아이디 파라미터명 설정
+    .passwordParameter("password") // 패스워드 파라미터명 설정
+    .loginProcessingUrl("/login") // 로그인 Form Action Url
+    .successHandler(loginSuccessHandler()) // 로그인 성공 후 핸들러
+    .failureHandler(loginFailureHandler()) // 로긍니 실패 후 핸들러
 }
 ```
--- src/main/java/security/demo/config/SecurityConfig.java
+++ src/main/java/security/demo/config/SecurityConfig.java
@@ -4,6 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -13,8 +22,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().authenticated() //요청에 대한 보안검사 시작 (인가정책)
-        .and()
-            .formLogin(); //form login 방식의 인증
+                .anyRequest().authenticated(); //요청에 대한 보안검사 시작 (인가정책)
+        http
+            .formLogin() //form login 방식의 인증
+            //.loginPage("/loginPage")
+            .defaultSuccessUrl("/")
+            .failureUrl("/login")
+            .usernameParameter("userId")
+            .passwordParameter("passwd")
+            .loginProcessingUrl("/login_proc")
+            .successHandler((request, response, authentication) -> {
+                System.out.println("authentication :"+authentication.getName());
+                response.sendRedirect("/");
+            }).failureHandler((request, response, exception) -> {
+                System.out.println("exception :"+ exception.getMessage());
+                response.sendRedirect("/login");
+            })
+                .permitAll(); //loginPage에 접근하는 클라이언트들은 인증받지않아도 가능
     }
 }
