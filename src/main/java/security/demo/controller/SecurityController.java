330119f86f56644cf231087b5586c41e5e6a6bc3
김준성 <anima94@naver.com>
2021-01-24 11:19:33 +0900

    2-4)인증저장소SecurityContextHolder

--- src/main/java/security/demo/controller/SecurityController.java
+++ src/main/java/security/demo/controller/SecurityController.java
@@ -1,37 +1,35 @@
 package security.demo.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class SecurityController {
 
     @GetMapping("/")
-    public String index(){
+    public String index(HttpSession session){
+        Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
+        SecurityContext context=(SecurityContext)session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        Authentication authentication1=context.getAuthentication();
         return "호옹이!";
     }
 
-    @GetMapping("/user")
-    public String user(){
-        return "user";
+    @GetMapping("/thread")
+    public String thread(){
+        new Thread(
+                () -> {
+                    Authentication authentication=SecurityContextHolder.getContext().getAuthentication();
+                }
+        ).start();
+        return "thread";
     }
 
-    @GetMapping("/admin/pay")
-    public String adminPay(){
-        return "adminpay";
-    }
 
-    @GetMapping("/admin/**")
-    public String admin(){
-        return "admin";
-    }
 
-    @GetMapping("/login")
-    public String login(){
-        return "login";
-    }
-    @GetMapping("/denied")
-    public String denied(){
-        return "Access is denied";
-    }
 }
