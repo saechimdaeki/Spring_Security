cd8e65fb422bf7883b6bf726591c70b7ee40995a
saechimdaeki <anima94@kakao.com>
2021-02-02 20:55:41 +0900

    [setting] webIgnore + properties
    postgresql은 안쓸래 귀찮......

--- Project/src/main/java/com/example/demo/controller/admin/SecurityConfig.java
+++ Project/src/main/java/com/example/demo/controller/admin/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller.admin;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -19,6 +21,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         String password=passwordEncoder().encode("1111");
 
@@ -26,10 +33,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .password(password).roles("USER");
 
         auth.inMemoryAuthentication().withUser("manager")
-                .password(password).roles("MANAGER");
+                .password(password).roles("MANAGER","USER");
 
         auth.inMemoryAuthentication().withUser("admin")
-                .password(password).roles("ADMIN");
+                .password(password).roles("ADMIN","USER","MANAGER");
     }
 
     @Override
