4d51802aaf8dae583ab92f70d6d8afb984dfac7b
saechimdaeki <anima94@kakao.com>
2021-02-07 12:01:27 +0900

    [feat]. DB연동 인증처리

--- Project/src/main/java/com/example/demo/security/configs/SecurityConfig.java
+++ Project/src/main/java/com/example/demo/security/configs/SecurityConfig.java
@@ -1,19 +1,36 @@
-package com.example.demo.security;
+package com.example.demo.security.configs;
 
+import com.example.demo.security.provider.CustomAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Bean
+    private AuthenticationProvider authenticationProvider() {
+        return new CustomAuthenticationProvider();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -25,19 +42,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        String password=passwordEncoder().encode("1111");
-
-        auth.inMemoryAuthentication().withUser("user")
-                .password(password).roles("USER");
-
-        auth.inMemoryAuthentication().withUser("manager")
-                .password(password).roles("MANAGER","USER");
-
-        auth.inMemoryAuthentication().withUser("admin")
-                .password(password).roles("ADMIN","USER","MANAGER");
-    }
+    /* ㅁㅔ모리 방식의 사용자 인증a*/
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        String password=passwordEncoder().encode("1111");
+//
+//        auth.inMemoryAuthentication().withUser("user")
+//                .password(password).roles("USER");
+//
+//        auth.inMemoryAuthentication().withUser("manager")
+//                .password(password).roles("MANAGER","USER");
+//
+//        auth.inMemoryAuthentication().withUser("admin")
+//                .password(password).roles("ADMIN","USER","MANAGER");
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
